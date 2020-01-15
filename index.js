b74c40560a07ffa2ea14adb870018d43787d0c11
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-01-15 11:25:56 -0700

    matterJs boiler plate added and perimeter walls built-ending #276

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('hi there');
+//boiler plate to access the objects in matter.js
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+//size of canvas element
+const width = 600;
+const height = 600;
+
+//more boiler plate to create and gain access to
+//matter js objects-more detailed explanation
+//in Bear notes
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+	element : document.body,
+	engine  : engine,
+	options : {
+		wireframes : false,
+		width,
+		height
+	}
+});
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+//walls-the perimeter of the maze
+const walls = [
+Bodies.rectangle(width / 2, 0, width, 40, { isStatic = true }),
+Bodies.rectangle(width / 2, height, width, 40, { isStatic = true }),
+Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+Bodies.rectangle(width, height / 2, 40, height, { isStatic: true}),
+];
+World.add(world, walls);
--- readMe.md
+++
@@ -1,6 +0,0 @@
-notRealCode, just #putting something in here
-so that I can practice with git
-
-Working on develop branch now
-
-This is a new feature.  Amazing!
