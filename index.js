287b2ae4a3082bae58981390916232a5267bc188
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-01-15 11:50:07 -0700

    added nested for loop to generate 3x3 grid with a console log test but there is a more concise solution so this is about to be deleted

--- index.js
+++ index.js
@@ -22,11 +22,28 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-//walls-the perimeter of the maze
+//walls-the perimeter of the maze.  Using constants for canvas height
+//and width, and using those constants in the walls, makes the walls
+//flexible so that they auto adapt if/when canvas size changes
 const walls = [
-Bodies.rectangle(width / 2, 0, width, 40, { isStatic = true }),
-Bodies.rectangle(width / 2, height, width, 40, { isStatic = true }),
-Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-Bodies.rectangle(width, height / 2, 40, height, { isStatic: true}),
+	Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+	Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+	Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+	Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
 ];
 World.add(world, walls);
+
+//Maze generation, starting with a blank array and using a double nested
+//for loop
+const grid = [];
+
+for (let i = 0; i < 3; i++) {
+	//in outerloop,push in each individual row
+	grid.push([]);
+	//in the inner loop, process all the rows and
+	//add in starting values of false
+	for (let j = 0; j < 3; j++) {
+		grid[i].push(false);
+	}
+}
+console.log(grid);
